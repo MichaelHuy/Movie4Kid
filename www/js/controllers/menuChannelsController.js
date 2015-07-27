2f6df314c494b06adb37be60fd7b561a865aafd0
MichaelHuy <lehuydit@gmail.com>
2015-07-27 07:12:38 +0700

    network checked

--- www/js/controllers/menuChannelsController.js
+++ www/js/controllers/menuChannelsController.js
@@ -1,6 +1,25 @@
 FFK
-.controller('MenuKidChannelsCtrl', function($rootScope, $scope, $http, $log, $ionicLoading, DatabaseService, $state) {
-    $ionicLoading.show();
+.controller('MenuKidChannelsCtrl', function($rootScope, $scope, $http, $log, $ionicLoading, DatabaseService, $state, $cordovaNetwork) {
+    
+    var type = $cordovaNetwork.getNetwork();
+
+    var isOnline = $cordovaNetwork.isOnline();
+
+    alert(type);
+    alert(isOnline);
+
+    // listen for Online event
+    $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+      var onlineState = networkState;
+    })
+
+    // listen for Offline event
+    $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+      var offlineState = networkState;
+    })
+    
+    if (isOnline) {
+            $ionicLoading.show();
     DatabaseService.getKidChannelsMenu().success( function (data) {
         $scope.items = data;
           $log.info(JSON.stringify($scope.item));
@@ -10,7 +29,9 @@ FFK
         $ionicLoading.hide();
         //alert("Please check network or turn on 3G");
         $log.info('Search error');
-      });
+      });        
+    }
+    
     $scope.clickOnItemOfMenu = function(item) {
       $rootScope.currentKidChannelPlaylist = item.playlistId;
       $rootScope.titleKidChannel= item.name;
